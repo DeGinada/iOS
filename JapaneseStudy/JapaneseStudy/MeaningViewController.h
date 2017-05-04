2780de655cac60584775af5cce2593a11bc85f6c
DeGinada <deginada@gmail.com>
2017-05-04 22:57:02 +0900

    170504

---
+++ JapaneseStudy/JapaneseStudy/MeaningViewController.h
@@ -0,0 +1,20 @@
+//
+//  MeaningViewController.h
+//  JapaneseStudy
+//
+//  Created by DeGi on 2017. 5. 4..
+//  Copyright © 2017년 DeGi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MeaningViewController : UIViewController
+
+@property IBOutlet UILabel* lbMeaning;
+@property IBOutlet UILabel* lbCount;
+@property IBOutlet UIButton* btnAnswer1;
+@property IBOutlet UIButton* btnAnswer2;
+@property IBOutlet UIButton* btnAnswer3;
+@property IBOutlet UIButton* btnAnswer4;
+
+@end
