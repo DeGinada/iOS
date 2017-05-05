17c65ada553a64eddf6b0643ebf763f11bb74dc7
DeGinada <deginada@gmail.com>
2017-05-05 23:43:37 +0900

    170505 1

---
+++ JapaneseStudy/JapaneseStudy/WordTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  WordTableViewController.h
+//  JapaneseStudy
+//
+//  Created by Sora Yeo on 2017. 5. 5..
+//  Copyright © 2017년 DeGi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WordTableViewController : UIViewController
+
+@end
---
+++ JapaneseStudy/JapaneseStudy/WordTableViewController.m
@@ -0,0 +1,37 @@
+//
+//  WordTableViewController.m
+//  JapaneseStudy
+//
+//  Created by Sora Yeo on 2017. 5. 5..
+//  Copyright © 2017년 DeGi. All rights reserved.
+//
+
+#import "WordTableViewController.h"
+
+@interface WordTableViewController ()
+
+@end
+
+@implementation WordTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
