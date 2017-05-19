5b276fb23ab60417fab0e0aec0880821aedf3511
DeGinada <deginada@gmail.com>
2017-05-19 23:57:43 +0900

    170519

---
+++ MusicAppTest/MusicAppTest/ResultViewController.h
@@ -0,0 +1,13 @@
+//
+//  ResultViewController.h
+//  MusicAppTest
+//
+//  Created by Sora Yeo on 2017. 5. 19..
+//  Copyright © 2017년 DeGi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ResultViewController : UITableViewController
+
+@end
--- MusicAppTest/MusicAppTest/SearchViewController.m
+++ MusicAppTest/MusicAppTest/SearchViewController.m
@@ -7,8 +7,16 @@
 //
 
 #import "SearchViewController.h"
+#import "ResultViewController.h"
 
-@interface SearchViewController ()
+@interface SearchViewController () 
+
+@property (nonatomic, strong) UISearchController* searchController;
+@property (nonatomic, strong) ResultViewController* resultTableVC;
+@property IBOutlet UITableView* tableView;
+
+@property BOOL searchControllerWasActive;
+@property BOOL searchControllerSearchFieldWasFirstResponder;
 
 @end
 
@@ -17,6 +25,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [self setBasicTableview];
 }
 
 
@@ -25,5 +35,69 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+- (void) viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+    if (self.searchControllerWasActive) {
+        self.searchController.active = self.searchControllerWasActive;
+        _searchControllerWasActive = NO;
+        
+        if (self.searchControllerSearchFieldWasFirstResponder) {
+            [self.searchController.searchBar becomeFirstResponder];
+            _searchControllerSearchFieldWasFirstResponder = NO;
+        }
+    }
+}
+
+
+// [170519] table view를 설정한다.
+- (void) setBasicTableview {
+    
+    _resultTableVC = [[ResultViewController alloc] init];
+    _searchController = [[UISearchController alloc] initWithSearchResultsController:self.resultTableVC];
+    self.searchController.searchResultsUpdater = self;
+    [self.searchController.searchBar sizeToFit];
+    self.tableView.tableHeaderView = self.searchController.searchBar;
+    
+    self.resultTableVC.tableView.delegate = self;
+    self.searchController.delegate = self;
+    self.searchController.dimsBackgroundDuringPresentation = NO;
+    self.searchController.searchBar.delegate = self;
+    
+    self.definesPresentationContext = YES;
+}
+
+
+
+#pragma mark - TABLE_VIEW
+
+
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    
+    return 3;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    static NSString* identifier = @"Cell";
+    UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:identifier];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier];
+    }
+    
+    
+    return cell;
+}
+
+
+#pragma mark - SEARCH_RESULT
+
+- (void)updateSearchResultsForSearchController:(UISearchController *)searchController {
+    
+}
 
 @end
