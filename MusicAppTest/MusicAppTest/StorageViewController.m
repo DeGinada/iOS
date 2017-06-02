10950b1ef00f3637556c370b8dd7e05d0fa2ef4c
DeGinada <deginada@gmail.com>
2017-06-02 20:47:22 +0900

    170602

--- MusicAppTest/MusicAppTest/StorageViewController.m
+++ MusicAppTest/MusicAppTest/StorageViewController.m
@@ -40,9 +40,9 @@
     UIView* viewHeader = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.tableView.frame.size.width, 88)];
     [viewHeader setBackgroundColor:[UIColor whiteColor]];
     
-    UILabel* lbTitle = [[UILabel alloc] initWithFrame:CGRectMake(15, 20, self.tableView.frame.size.width-30, 68)];
+    UILabel* lbTitle = [[UILabel alloc] initWithFrame:CGRectMake(18, 20, self.tableView.frame.size.width-36, 68)];
     [lbTitle setText:@"보관함"];
-    [lbTitle setFont:[UIFont boldSystemFontOfSize:32]];
+    [lbTitle setFont:[UIFont boldSystemFontOfSize:34]];
     [lbTitle setTextColor:[UIColor blackColor]];
     [lbTitle setTextAlignment:NSTextAlignmentLeft];
     [viewHeader addSubview:lbTitle];
@@ -98,6 +98,12 @@
 }
 
 
+// [170602] album 상세 화면으로 간다
+- (void) goAlbumDetail:(id)sender {
+    
+}
+
+
 - (void) setTableFooterView {
     
     // 정보 가져오기
@@ -143,14 +149,73 @@
     // footer view 높이 정해주기
     float fFooterHeight = 0.0;
     if (allAlbums.count > 20) {
-        fFooterHeight = 65+(216*20);
+        fFooterHeight = 65+(216*10);
     } else {
         long nHeight = ((allAlbums.count)+1)/2;
         fFooterHeight = 65+(216*nHeight);
     }
     
-    self.tableView.tableFooterView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.tableView.frame.size.width, fFooterHeight)];
     
+    // footerview 만들기
+    UIView* viewFooter = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.tableView.frame.size.width, fFooterHeight)];
+    [viewFooter setBackgroundColor:[UIColor whiteColor]];
+    
+    
+    // 타이틀
+    UILabel* lbTitle = [[UILabel alloc] initWithFrame:CGRectMake(18, 13, self.tableView.frame.size.width-36, 52)];
+    [lbTitle setText:@"최근 추가된 항목"];
+    [lbTitle setTextColor:[UIColor blackColor]];
+    [lbTitle setFont:[UIFont boldSystemFontOfSize:18]];
+    [viewFooter addSubview:lbTitle];
+    
+    
+    // 버튼 정렬
+    int nCount = 0;
+    for (MPMediaItemCollection* item in allAlbums) {
+        
+        // nCount가 짝수면 left, 홀수면 right
+        // nCount/2로 해서 나온 몫이 높이 카운트
+        UIButton* btnAlbum = [[UIButton alloc] initWithFrame:CGRectMake(nCount%2 == 0 ? 20 : 197, 65+((nCount/2)*216), 158, 216)];
+        [btnAlbum setImage:(UIImage*)item forState:UIControlStateDisabled];
+        [btnAlbum addTarget:self action:@selector(goAlbumDetail:) forControlEvents:UIControlEventTouchUpInside];
+        [viewFooter addSubview:btnAlbum];
+        
+        
+        UIImageView* imgAlbum = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 158, 158)];
+        [imgAlbum setImage:nil];
+        [imgAlbum setBackgroundColor:[UIColor whiteColor]];
+        imgAlbum.layer.cornerRadius = 5;
+        imgAlbum.layer.borderWidth = 0.1;
+        imgAlbum.layer.borderColor = [UIColor lightGrayColor].CGColor;
+        imgAlbum.clipsToBounds = YES;
+        [btnAlbum addSubview:imgAlbum];
+        
+        
+        UILabel* lbTitle = [[UILabel alloc] initWithFrame:CGRectMake(0, 162, 158, 19)];
+        [lbTitle setText:@"제목"];
+        [lbTitle setTextColor:[UIColor blackColor]];
+        [lbTitle setFont:[UIFont systemFontOfSize:15]];
+        [lbTitle setTextAlignment:NSTextAlignmentLeft];
+        [lbTitle setLineBreakMode:NSLineBreakByCharWrapping];
+        [btnAlbum addSubview:lbTitle];
+        
+        UILabel* lbArtist = [[UILabel alloc] initWithFrame:CGRectMake(0, 182, 158, 19)];
+        [lbArtist setText:@"아티스트"];
+        [lbArtist setTextColor:[UIColor lightGrayColor]];
+        [lbArtist setFont:[UIFont systemFontOfSize:15]];
+        [lbArtist setTextAlignment:NSTextAlignmentLeft];
+        [lbArtist setLineBreakMode:NSLineBreakByCharWrapping];
+        [btnAlbum addSubview:lbArtist];
+        
+        
+        nCount++;
+        if (nCount >= 20) {
+            break;
+        }
+    }
+    
+    
+    self.tableView.tableFooterView = viewFooter;
 }
 
 
@@ -205,7 +270,8 @@
     
     [cell.textLabel setText:[self.arListTitle objectAtIndex:indexPath.row]];
     [cell.textLabel setTextColor:RED_COLOR];
-    [cell.textLabel setFont:[UIFont systemFontOfSize:18]];
+    [cell.textLabel setFont:[UIFont systemFontOfSize:20]];
+    [cell.textLabel setFrame:CGRectMake(20, cell.textLabel.frame.origin.y, cell.textLabel.frame.size.width, cell.textLabel.frame.size.height)];
     
     
     return cell;
