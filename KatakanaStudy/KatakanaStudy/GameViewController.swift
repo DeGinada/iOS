82dadc1978c4e8a6f91323253301b09f1273141b
DeGinada <deginada@gmail.com>
2017-07-10 23:20:35 +0900

    170710

--- KatakanaStudy/KatakanaStudy/GameViewController.swift
+++ KatakanaStudy/KatakanaStudy/GameViewController.swift
@@ -74,13 +74,88 @@ class GameViewController: UIViewController {
         let quizNum:Int = Int(arc4random()) % g_arKanakana.count
         lbKatakana.tag = quizNum
         
+        // array에서 dic으로 가져온 후, string 적용
         let quiz:NSDictionary = g_arKanakana.object(at: quizNum) as! NSDictionary
         lbKatakana.text? = quiz.object(forKey: "ka") as! String
         
+        let arAnswer:Array<String> = suffleAnswer()
         
+        btnAnswer1.setTitle(arAnswer[0], for: .normal)
+        btnAnswer2.setTitle(arAnswer[1], for: .normal)
+        btnAnswer3.setTitle(arAnswer[2], for: .normal)
+        btnAnswer4.setTitle(arAnswer[3], for: .normal)
+        
+    }
+    
+    
+    func suffleAnswer() -> Array<String> {
+        
+        var answer:Array<String> = ["", "", "", ""]
+        let numAnswer:Int = Int(arc4random()%4)
+    
+        let quiz:NSDictionary = g_arKanakana.object(at: lbKatakana.tag) as! NSDictionary
+        if (g_isHiragana) {
+//            answer.insert(quiz.object(forKey: "hi") as! String, at: numAnswer)
+            answer[numAnswer] = quiz.object(forKey: "hi") as! String
+        } else {
+//            answer.insert(quiz.object(forKey: "en") as! String, at: numAnswer)
+            answer[numAnswer] = quiz.object(forKey: "en") as! String
+        }
+        
+        var count:Int = 0
+        while (count < 4) {
+            if (count != numAnswer) {
+                let index:Int = Int(arc4random())%g_arKanakana.count
+                let temp:NSDictionary = g_arKanakana.object(at: index) as! NSDictionary
+                var strTemp:String = ""
+                if (g_isHiragana) {
+                    strTemp = temp.object(forKey: "hi") as! String
+                } else {
+                    strTemp = temp.object(forKey: "en") as! String
+                }
+                
+                var isExist:Bool = false
+                
+                for str:String in answer {
+                    if (strTemp.compare(str) == .orderedSame) {
+                        isExist = true
+                    }
+                }
+                
+                if (!isExist) {
+//                    answer.insert(strTemp, at: count)
+                    answer[count] = strTemp
+                    count += 1
+                }
+            } else {
+                count += 1
+            }
+        }
+        
+        return answer
     }
 
     @IBAction func checkAnswer(sender: AnyObject) {
         // 정답확인 함수
+        let button = sender as! UIButton
+        
+        
+        let quiz:NSDictionary = g_arKanakana.object(at: lbKatakana.tag) as! NSDictionary
+        var quizAnswer:String = ""
+        if (g_isHiragana) {
+            quizAnswer = quiz.object(forKey: "hi") as! String
+        } else {
+            quizAnswer = quiz.object(forKey: "en") as! String
+        }
+        
+        if let userAnswer = button.title(for: .normal) {
+            if (quizAnswer.compare(userAnswer) == .orderedSame) {
+                NSLog("정답입니다", "")
+                
+                showKatakana()
+            } else {
+                NSLog("땡!!!!", "")
+            }
+        }
     }
 }
